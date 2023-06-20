2c0aa6973e0c6a6ac5ee88cc503f239761e23653
Alvaro2003j <93229518+Alvaro2003j@users.noreply.github.com>
2023-06-20 18:58:39 -0500

    Add files via upload

---
+++ finanzas-back-end/src/main/java/pe/edu/upc/finanzasbackend/FinanzasBackEndApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.finanzasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzasBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzasBackEndApplication.class, args);
+	}
+
+}
